8a7ecaa514a37fe4a05e3741d3eddb0a3110ee18
epittab <ep7m@me.com>
2020-10-24 19:35:52 -0400

    working through composition and encapsulation

--- Computer/Main.java
+++ Computer/Main.java
@@ -4,5 +4,9 @@ public class Main {
     
     public static void main(String[] args) {
         //code
+        Monitor monitor = new Monitor("VG2455", "ViewSonic", new Resolution(1920, 1080));
+        monitor.getManufacturer();
+        monitor.getModel();
+        monitor.getRes();
     }
 }
--- Computer/Monitor.java
+++ Computer/Monitor.java
@@ -1,5 +1,25 @@
 package com.OOP_advanced.Computer;
 
 public class Monitor {
-    
+
+    private String model;
+    private String manufacturer;
+    private Resolution resolution;
+
+    public Monitor(String model, String manufacturer, Resolution resolution){
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.resolution = resolution;
+    }
+
+    public void getModel(){
+        System.out.println("The model is " + model);
+    }
+
+    public void getManufacturer(){
+        System.out.println("The manufacturer is " + manufacturer);
+    }
+    public void getRes(){
+        System.out.println("The resolution is " + resolution.getWidth() + " x " + resolution.getHeight());
+    }
 }
